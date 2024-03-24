64aaee285a6b9be8d523745364c70219e4319983
Taavetl <163862539+Taavetl@users.noreply.github.com>
2024-03-24 15:16:32 +0200

    Add files via upload

---
+++ MyApp/MyApp/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+Console.WriteLine("Enter your name:");
+
+String username;
+
+username = Console.ReadLine();
+//Console.WriteLine("Yo" + " " + username);
+Console.WriteLine($"yo, {username}!"); 
+
+
+
+
+
